ac008f08677db8bffef5b5f6a897b36e45f32478
SoulViolin <trofimencko.ego@yandex.ru>
2023-10-26 01:19:28 +0300

    second commit

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import sqlite3
 
 bot = telebot.TeleBot(token, parse_mode=None)
 
-def DBMS_Connection(command, values=''):
+def DBMS_Connection(query, values=''):
     conn = sqlite3.connect('users.db')
     cur = conn.cursor()
 
-    cur.execute(command, values)
+    cur.execute(query, values)
     result = cur.fetchone()
 
     conn.commit()
@@ -17,6 +17,45 @@ def DBMS_Connection(command, values=''):
     conn.close()
 
     return result
+
+# Объединенная функция регистрации и авторизации
+def RegAuth(message, mode):
+    bot.send_message(message.chat.id, "Введите логин:")
+    bot.register_next_step_handler(message, process_name_reg_auth, mode)
+
+def process_name_reg_auth(message, mode):
+    login = message.text
+    result = DBMS_Connection('SELECT * FROM users WHERE username = ?', (login,))
+    
+    if mode == 'reg':    
+        if result is not None:
+            msg = bot.send_message(message.chat.id, f'Пользователь с логином {login} уже существует')
+        else:
+            bot.send_message(message.chat.id, "Введите пароль:")
+            bot.register_next_step_handler(message, process_password_reg_auth, login, mode)
+    elif mode == 'log':
+        if result is None:
+            msg = bot.send_message(message.chat.id, f'Пользователя с таким логином {login} не существует')
+        else:
+            bot.send_message(message.chat.id, "Введите пароль:")
+            bot.register_next_step_handler(message, process_password_reg_auth, login, mode)
+
+def process_password_reg_auth(message, login, mode):
+    password = message.text
+
+    if mode == 'reg':
+        DBMS_Connection(f"""
+            INSERT INTO users (chat_id, username, password, role) VALUES (?, ?, ?, ?)""",
+            (message.chat.id, login, password, 'student')
+            )
+        bot.send_message(message.chat.id, "Вы успешно зарегистрированы")
+    elif mode == 'log':
+        stored_password = DBMS_Connection("SELECT password FROM users WHERE username = ?", (login,))[0]
+        
+        if stored_password == password:
+            bot.send_message(message.chat.id, "Вы успешно авторизовались")
+        else:
+            bot.send_message(message.chat.id, "Неверный пароль")
 
 DBMS_Connection("""
     CREATE TABLE IF NOT EXISTS users (
@@ -31,51 +70,20 @@ DBMS_Connection("""
 # =========================
 @bot.message_handler(commands=['start'])
 def start_handler(message):
-    msg = bot.send_message(message.chat.id, "Здравствуйте")
-    msg = bot.send_message(message.chat.id, "Если у Вас уже есть аккаунт, введите /login. \nЕсли нет - /register")
+    bot.send_message(message.chat.id, "Здравствуйте\nЕсли у Вас уже есть аккаунт, введите /login. \nЕсли нет - /register")
 
 # =========================
+# Обработка команды регестрации
 @bot.message_handler(commands=['register'])
 def register_handler(message):
-    msg = bot.send_message(message.chat.id, "Введите логин")
-    bot.register_next_step_handler(msg, process_username_step)
+    RegAuth(message, 'reg')
 
-# Обработчик ввода логина
-def process_username_step(message):
-    try:
-        username = message.text
-
-        result = DBMS_Connection('SELECT * FROM users WHERE username = ?', (username))
-
-        if result is not None:
-            msg = bot.send_message(message.chat.id, f'Пользователь с логином {username} уже существует')
-        else:
-            # Запрашиваем пароль
-            msg = bot.send_message(message.chat.id, "Введите пароль:")
-            bot.register_next_step_handler(msg, process_password_step, username)
-    except Exception as e:
-        print(e)
-        bot.reply_to(message, 'Ошибка, повторите попытку.')
-
-# Обработчик ввода пароля
-def process_password_step(message, username):
-    try:
-        password = message.text
-
-    except Exception as e:
-        bot.reply_to(message, 'Ошибка, повторите попытку.')
-    try:
-        DBMS_Connection(f"""
-        INSERT INTO users (chat_id, username, password, role) VALUES (?, ?, ?, ?)""",
-        (message.chat.id, username, password, 'student')
-        )
-        msg = bot.send_message(message.chat.id, "Вы успешно зарегистрированы")
-    except Exception as e:
-        bot.reply_to(message, 'Ошибка, повторите попытку.')
-    
 # =========================
+# Обработка команды авторизации
 @bot.message_handler(commands=['login'])
 def login_handler(message):
-    msg = bot.send_message(message.chat.id, "Введите логин")
+    RegAuth(message, 'log')
+
+# =========================
 
 bot.polling(none_stop=True)
